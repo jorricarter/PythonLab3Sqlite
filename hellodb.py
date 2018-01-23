549b2be4d6fec539a878db670be2557207f0fecd
Jorri <Carter>
2018-01-23 14:40:14 -0600

    Initial commit

---
+++ hellodb.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+db = sqlite3.connect('my_first_db.db')  # Creates or opens a databsase file
+
+cur = db.cursor()  # Need a cursor object to perform operations
+
+# Create a table
+cur.execute('create table if not exists phones (brand text, version int)')
+
+# # Add some data
+# cur.execute('insert into phones values ("Android", 5)')
+# cur.execute('insert into phones values ("iPhone", 6)')
+#
+
+# Ask user for information for a new phone
+brand = input('Enter brand of phone: ')
+version = int(input('Enter version of phone (as an integer): '))
+
+# Parameters. Use a ? as a placeholder for data that will be filled in
+# Provide data as a second argument to .execute, as a tuple of values
+cur.execute('insert into phones values (?, ?)', (brand, version))
+
+# # Execute a query. Results are contained in cursor object
+# for row in cur.execute('select * from phones'):
+#     print(row)
+#
+
+# Execute a query. Results are contained in cursor object
+results = cur.execute("select * from phones")
+for row in results:
+    print(row)
+
+cur.execute('drop table phones')  # Delete table
+
+db.commit()  # Ask the database to save changes
+
+db.close()
---
+++ list_comp.py
@@ -0,0 +1,20 @@
+my_list = [-45, 1, 4, -4, 34, 3]
+# Write code to create a new list with just the positive numbers
+positive = [x for x in my_list if x >= 0]
+# New list with all the numbers doubled
+double = [x * 2 for x in my_list]
+# New list with all the positive numbers doubled
+double_positive = [x * 2 for x in my_list if x >= 0]
+
+words = [ 'hello', 'world', 'python', 'kittens']
+# Make new list of all strings in uppercase.
+uppercase = [w.upper() for w in words]
+
+languages = ['JS', 'C#', 'Java', 'visual Basic.NET']
+# todo make new list of all strings longer than 3 letters
+longer_than_three_letters = [w for w in words if len(w) > 3]
+# todo make new list of all strings longer than 3 letters converted
+# to uppercase ['JAVA', 'VISUAL MASIC.NET']
+longer_than_three_letters_upper = [w.upper() for w in words if len(w) > 3]
+lowercase_languages = [l.lower() for l in languages]
+print(lowercase_languages)
